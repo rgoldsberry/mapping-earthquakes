894abbdf619e7f673368a9a33e92cfbd6426244a
rgoldsberry <107135474+rgoldsberry@users.noreply.github.com>
2022-09-11 14:49:49 -0600

    added popup to each earthquake marker

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -34,7 +34,7 @@ function styleInfo(feature) {
     return {
         opacity : 1,
         fillOpacity : 1,
-        fillColor : "#ffae42",
+        fillColor : getColor(feature.properties.mag),
         color : "#000000",
         radius : getRadius(feature.properties.mag),
         stroke : true,
@@ -50,14 +50,39 @@ function getRadius(magnitude) {
     return magnitude * 4;
 }
 
+function getColor(magnitude) {
+    if (magnitude > 5) {
+      return "#ea2c2c";
+    }
+    if (magnitude > 4) {
+      return "#ea822c";
+    }
+    if (magnitude > 3) {
+      return "#ee9c00";
+    }
+    if (magnitude > 2) {
+      return "#eecc00";
+    }
+    if (magnitude > 1) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
+  };
+
 // accessing the USGS earthquake geoJSON
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
     console.log(data);
     // build the layer
     L.geoJSON(data, {
+        // make a circleMarker from each feature
         pointToLayer: function(feature, latlng) {
             return L.circleMarker(latlng);
         },
-        style : styleInfo
+        // set the style
+        style : styleInfo ,
+        // create the popup for each feature
+        onEachFeature : function(feature, layer) {
+            layer.bindPopup(`Magnitude: ${feature.properties.mag}<br>Location: ${feature.properties.place}`)
+        }
     }).addTo(map);
 })
